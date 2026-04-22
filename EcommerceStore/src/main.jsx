cbdeb34cb7e383afb963a6eec2530e29706380cd
aqsa-eng <nmalbaig@gmail.com>
2026-04-21 23:59:51 -0700

    added new components

--- EcommerceStore/src/main.jsx
+++ EcommerceStore/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './assets/Components/login.jsx'
+import Dashboard from './assets/Components/dashboard.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+       <Route path='/login' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+    </Routes>
+  </BrowserRouter>,
 )
